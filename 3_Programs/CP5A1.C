b5cb03457629b546fcff8fd721dfdba1a0fba4d3
AMROLI BCA <contact2hitesh@gmail.com>
2023-10-16 08:40:02 +0530

    New Program Added...!!!

--- 3_Programs/CP5A1.C
+++ 3_Programs/CP5A1.C
@@ -1,25 +1,21 @@
 #include<stdio.h>
 #include<conio.h>
 void main()
-
 {
-int n,i;
-int fybca4[1];
-printf("Please Enter Size of Array:\n");
-scanf("%d",&n);
-fybca4[n];
+int i,fybca8[]={816,860};
+clrscr();
+//	printf("STORING DATA IN 1-D ARRAY:\n");
+//	for(i=0;i<5;i++)
+//	{
+//	printf("Please Enter your Data FYBCA8[%d]: \t",i);
+//	scanf("%d",&fybca8[i]);
+//	}
+	printf("Data Stored Success\n");
 
 
-	printf("Storing Data in 1-D Array:\n");
-	for(i=0;i<n;i++)
-	{
-	printf("\tEnter Ele at %d:",i);
-	scanf("%d",&fybca4[i]);
-	}
-	 printf("\nDISPLAYING CONTENTS OF ARRAY\n");
-	 for (i=0;i<n;i++)
-	 printf("fybca4[%d]=%d\t",i,fybca4[i]);
-
+	printf("\nPRINTING DATA IN 1-D ARRAY:\n");
+	for(i=0;i<10;i++)
+	printf("fybca8[%d]=%d\t",i,fybca8[i]);
 
 getch();
 }
---
+++ 3_Programs/PAP79ARR.C
@@ -0,0 +1,24 @@
+/*DOCUMENTATION SECTION
+PAP 79.WAP TO SHOW DEMO OF 1 D ARRAY:
+*/
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+int fybca7[10],i;
+clrscr();
+printf("PROGRAM TO STORE DATA IN ARRAY:\n");
+	for(i=0;i<10;i++)
+	{
+	printf("\nPlease Enter Element for fybca7[%d]= ",i);
+	scanf("%d",&fybca7[i]);
+	}
+printf("DISPLAYING STORED DATA IN ARRAY:\n");
+for(i=0;i<10;i++)
+{
+printf("ELEMENT AT fybca[%d]=%d\n",i,fybca7[i]);
+}
+getch();
+
+
+}
