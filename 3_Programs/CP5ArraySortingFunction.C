ff1251d4e7bd5e4aff7c7136c913d425c514933d
AMROLI BCA <contact2hitesh@gmail.com>
2023-10-26 18:27:50 +0530

    Array Sorting Step by Step

---
+++ 3_Programs/CP5ArraySortingFunction.C
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <conio.h>
+void showdata();
+int array[50];
+int k,l,n,m;
+int main()
+{
+    int i=0,j=0,temp=0;
+    printf("\nEnter Size for an Array (< 50): ");
+    scanf("%d", &n);
+    m=n;
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter Element for array[%d]= ", i);
+        scanf("%d", &array[i]);
+    }
+    printf("\nElements of Inputed Array:\n");
+    showdata();
+    printf("\nApplying Bubble Sorting : \n");
+    // CHECKING ELEMENTS AND SWAPPING IF ELEMENT IS GREATED THAN SECOND ELEMENT:
+    for (i = 0; i < n ; i++)
+    {
+        printf("\nPass %d, i=%d\n",i+1,i);
+        for (j = 0; j < n-1-i; j++)
+        {
+            printf("\nj=%d,j+1=%d \t",j,j+1);
+            if (array[j] > array[j+1])
+            {
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        showdata();
+        }
+        
+        
+
+    }
+        printf("\nArray Elements after Bubble Sorting : \n");
+        showdata();
+    
+}
+void showdata()
+{
+for (k = 0; k < n; k++)
+        {
+            printf("| %d ",array[k]);
+        }
+        printf("|\n");
+       // return;
+}
